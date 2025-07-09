0c13682382b6e7e3871d8a61efcbdaa2f9d511a5
Portmanteaux26 <michael.a.ba@gmail.com>
2025-07-09 16:15:48 -0500

    ensure autos don't make copies

--- Space Invaders/source/ResourceManager.cpp
+++ Space Invaders/source/ResourceManager.cpp
@@ -44,10 +44,10 @@ Texture2D ResourceManager::GetTexture(std::string name)
 void ResourceManager::Clear()
 {
     // (properly) delete all shaders	
-    for (auto iter : Shaders)
+    for (auto& iter : Shaders)
         glDeleteProgram(iter.second.ID);
     // (properly) delete all textures
-    for (auto iter : Textures)
+    for (auto& iter : Textures)
         glDeleteTextures(1, &iter.second.ID);
 }
 
