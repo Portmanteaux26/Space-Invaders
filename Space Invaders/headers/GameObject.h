53c338fa43cbb673fcacbbd28c2465e8479a207c
Portmanteaux26 <michael.a.ba@gmail.com>
2025-07-15 14:30:16 -0500

    const-ness and ref-ness check on GameObject

--- Space Invaders/headers/GameObject.h
+++ Space Invaders/headers/GameObject.h
@@ -1,41 +1,39 @@
-/*******************************************************************
-** This code is part of Breakout.
-**
-** Breakout is free software: you can redistribute it and/or modify
-** it under the terms of the CC BY 4.0 license as published by
-** Creative Commons, either version 4 of the License, or (at your
-** option) any later version.
-******************************************************************/
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 
-#include "texture.h"
+#include "Texture.h"
 #include "SpriteRenderer.h"
 
 
-// Container object for holding all state relevant for a single
-// game object entity. Each object in the game likely needs the
-// minimal of state as described within GameObject.
 class GameObject
 {
-public:
+private:
     // object state
-    glm::vec2 Position = glm::vec2(0.0f, 0.0f);
-    glm::vec2 Size = glm::vec2(0.0f, 0.0f);
     glm::vec2 Velocity = glm::vec2(0.0f);
     glm::vec3 Color = glm::vec3(1.0f);
     float Rotation = 0.0f;
     bool  IsSolid = false;
     bool  Destroyed = false;
     // render state
-    Texture2D   Sprite;
-    // constructor(s)
-    GameObject();
-    GameObject(glm::vec2 pos, glm::vec2 size, Texture2D sprite, glm::vec3 color = glm::vec3(1.0f), glm::vec2 velocity = glm::vec2(0.0f, 0.0f));
-    // draw sprite
+    const Texture2D& Sprite;
+
+public:
+    // object state
+    glm::vec2 Position = glm::vec2(0.0f, 0.0f);
+    glm::vec2 Size = glm::vec2(0.0f, 0.0f);
+    
+    // constructor
+    GameObject(
+        glm::vec2 pos,
+        glm::vec2 size,
+        const Texture2D& sprite,
+        glm::vec3 color = glm::vec3(1.0f),
+        glm::vec2 velocity = glm::vec2(0.0f, 0.0f)
+    );
+    // draws sprite
     virtual void Draw(SpriteRenderer& renderer);
 };
 
--- Space Invaders/source/GameObject.cpp
+++ Space Invaders/source/GameObject.cpp
@@ -1,17 +1,7 @@
-/*******************************************************************
-** This code is part of Breakout.
-**
-** Breakout is free software: you can redistribute it and/or modify
-** it under the terms of the CC BY 4.0 license as published by
-** Creative Commons, either version 4 of the License, or (at your
-** option) any later version.
-******************************************************************/
 #include "GameObject.h"
 
 
-GameObject::GameObject() { }
-
-GameObject::GameObject(glm::vec2 pos, glm::vec2 size, Texture2D sprite, glm::vec3 color, glm::vec2 velocity)
+GameObject::GameObject(glm::vec2 pos, glm::vec2 size, const Texture2D& sprite, glm::vec3 color, glm::vec2 velocity)
     : Position(pos)
     , Size(size)
     , Velocity(velocity)
