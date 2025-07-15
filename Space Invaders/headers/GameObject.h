54e0b9444c1eeb65eb0c08b56964ab5e012b29fc
Portmanteaux26 <michael.a.ba@gmail.com>
2025-07-15 13:51:39 -0500

    simplify GameObject constructor

--- Space Invaders/headers/GameObject.h
+++ Space Invaders/headers/GameObject.h
@@ -23,11 +23,13 @@ class GameObject
 {
 public:
     // object state
-    glm::vec2   Position, Size, Velocity;
-    glm::vec3   Color;
-    float       Rotation;
-    bool        IsSolid;
-    bool        Destroyed;
+    glm::vec2 Position = glm::vec2(0.0f, 0.0f);
+    glm::vec2 Size = glm::vec2(0.0f, 0.0f);
+    glm::vec2 Velocity = glm::vec2(0.0f);
+    glm::vec3 Color = glm::vec3(1.0f);
+    float Rotation = 0.0f;
+    bool  IsSolid = false;
+    bool  Destroyed = false;
     // render state
     Texture2D   Sprite;
     // constructor(s)
